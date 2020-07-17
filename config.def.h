58630cbb549a0fdeffc6c864ce3e6a073e3694a7
Danilo Blera <danilo.cblera@protonmail.com>
2020-07-17 08:51:05 -0300

    Add files

---
+++ config.def.h
@@ -0,0 +1,21 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#170420",   /* after initialization */
+	[INPUT] =  "#1e457e",   /* during input */
+	[FAILED] = "#ea1646",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 0;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ddfafa";
+
+/* text size (must be a valid size) */
+static const char * font_name = "9x15";
